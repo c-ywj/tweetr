35d14bf1c017b5c9ff0d9b101dbdf14fd649a514
c-ywj <cybc00@hotmail.com>
2017-02-23 00:38:02 +0000

    adds function to render hardcoded database; now appends jQuery objects onto the page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,49 +4,88 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    "handle": "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
-const renderTweets = () => {
-
+const renderTweets = (tweets) => {
+  // loops thru tweets
+  var tweetsContainer = $('#tweets-container');
+  tweets.forEach( (tweet) => {
+    const result = createTweetElement(tweet);
+    tweetsContainer.append(result);
+  })
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
 };
 
 const createTweetElement = (newTweet) => {
-  return `<article class="tweet">
-            <header>
-              <img src="${newTweet.user.avatars.small}">
-              <h2>${newTweet.user.name}</h2>
-              <span>${newTweet.user.handle}</span>
-            </header>
-            <p>
-              ${newTweet.content.text}
-            </p>
-            <footer>
-              <span>999 days ago</span>
-              <div class="footer-icons">
-                <i class="fa fa-flag" aria-hidden="true"></i>
-                <i class="fa fa-retweet" aria-hidden="true"></i>
-                <i class="fa fa-heart" aria-hidden="true"></i>
-              </div>
-            </footer>
-            </article>`;
+  return `
+    <article class="tweet">
+      <header>
+        <img src="${newTweet.user.avatars.small}">
+        <h2>${newTweet.user.name}</h2>
+        <span>${newTweet.user.handle}</span>
+      </header>
+      <p>
+        ${newTweet.content.text}
+      </p>
+      <footer>
+        <span>999 days ago</span>
+        <div class="footer-icons">
+          <i class="fa fa-flag" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+          <i class="fa fa-heart" aria-hidden="true"></i>
+        </div>
+      </footer>
+    </article>
+  `;
 }
 
 $(document).ready(function () {
-  const tweet = createTweetElement(tweetData);
-  $('#tweets-container').append(tweet);
+  renderTweets(data);
 });
 
